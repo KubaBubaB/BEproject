0a5c17e81c8fddf3783dc699b21918926bd60413
StanislawRak <111492712+StanislawRak@users.noreply.github.com>
2024-01-04 21:57:14 +0100

    Add files via upload

---
+++ docker/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IMAGE_NAME="alexander0077/biznes:latest"
+COMPOSE_URL="https://raw.githubusercontent.com/KubaBubaB/BEproject/main/docker/docker-compose.yml"
+INIT_URL="https://raw.githubusercontent.com/KubaBubaB/BEproject/main/docker/init_script.sh"
+STACK_NAME="BE_188672"
+
+echo "Pulling docker image..."
+docker pull $IMAGE_NAME
+
+echo "Downloading docker-compose.yml..."
+wget $COMPOSE_URL -O docker-compose.yml
+
+echo "Downloading init_script.sh..."
+wget $INIT_URL
+
+echo "Deploying the app..."
+docker stack deploy -c docker-compose.yml $STACK_NAME --with-registry-auth
