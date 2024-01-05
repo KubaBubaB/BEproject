8776afdb5dc1bf6c02cf1ac2ea9184b85ed9b1c9
Aleksander Sarzyniak <102765654+alexander0077@users.noreply.github.com>
2024-01-05 19:20:22 +0100

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME="kiwing132/prestashop:latest"
+IMAGE_NAME="kiwing132/biznes:latest"
 COMPOSE_URL="https://raw.githubusercontent.com/KubaBubaB/BEproject/main/docker/docker-compose.yml"
 INIT_URL="https://raw.githubusercontent.com/KubaBubaB/BEproject/main/docker/init_script.sh"
 STACK_NAME="BE_188672"
